39560882d136c997211cd28f1e015515feb9267b
Thammammm <gotham.0706@gmail.com>
2025-09-18 16:26:10 +0700

    3

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -3,6 +3,9 @@ item_list = sys.argv
 item_list.pop(0)
 i = len(sys.argv)
 x = 0
+if i == 0 :
+    print("none")
+    exit()
 print("parameters:",i)
 for x in range(i):
     print(f"{sys.argv[x]}: {len(sys.argv[x])}")
